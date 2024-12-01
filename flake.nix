86a42fc208a016d4300deba0ded69697d55eb8b1
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-12-01 21:54:41 +0100

    Add papermod theme and format with treefmt

--- flake.nix
+++ flake.nix
@@ -1,18 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    hugo-papermod = {
+      url = "github:adityatelange/hugo-papermod";
+      flake = false;
+    };
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      hugo-papermod,
+      treefmt-nix,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
     in
     {
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         name = "hugo-blog";
         src = self;
+        configurePhase = ''
+          mkdir -p themes/papermod
+          cp -r ${hugo-papermod}/* themes/papermod
+        '';
         buildPhase = ''
           ${pkgs.hugo}/bin/hugo --minify
         '';
@@ -22,7 +37,12 @@
         buildInputs = with pkgs; [
           hugo
         ];
+        shellHook = ''
+          mkdir -p themes
+          ln -sfn ${hugo-papermod} themes/papermod
+        '';
       };
-      formatter.${system} = pkgs.nixfmt-rfc-style;
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      checks.${system}.formatting = treefmtEval.config.build.check self;
     };
 }
